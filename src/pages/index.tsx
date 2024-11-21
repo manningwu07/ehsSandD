58bc105fc917159a205b6c12d897890a30b57554
Manning Wu <manningwu123@gmail.com>
2024-11-20 19:31:27 -0800

    Quick metadata fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>EHS Speech and Debate</title>
+        <title>Emerald Speech & Debate Club</title>
         <meta
           name="description"
           content="Emerald High School's Speech and Debate Club, empowering youth voices and fostering a culture of fair discourse."
         />
         <meta
           name="keywords"
-          content="EHS, Emerald High School, Speech, Debate, Dublin CA, High School, Club"
+          content="EHS, Emerald High School, Speech, Debate, Dublin CA, High School, Club, Emerald Speech & Debate Club"
         />
-        <meta name="author" content="EHS Speech and Debate, Manning Wu" />
+        <meta name="author" content="Emerald Speech & Debate Club, Manning Wu" />
         <meta
           name="google-site-verification"
           content="A6YrIHHiVDJIA8O5_fVIFMNVm8W7lxnVZd6TYIy5w4Q"
